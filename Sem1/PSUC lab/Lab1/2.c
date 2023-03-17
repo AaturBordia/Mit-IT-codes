d3fa99c98e2701c3dbc784f881633e978a130cb4
Aatur Bordia <127979938+AaturBordia@users.noreply.github.com>
2023-03-18 02:01:05 +0530

    Update 2.c

--- Sem1/PSUC lab/Lab1/2.c
+++ Sem1/PSUC lab/Lab1/2.c
@@ -3,16 +3,19 @@
 
 int main()
 {
-    int a,b,sum,diff,quo,pro;
+    int a,b,sum,diff,quo,pro,rem;
     printf("Enter any two numbers\n");
     scanf("%d%d",&a,&b);
     sum=a+b;
     diff=a-b;
     pro=a*b;
     quo=a/b;
+    rem=a%b;
     printf("The sum of two numbers is %d",sum );
     printf("\nThe difference of the two numbers is %d",diff);
     printf("\nThe product of the two numbers is %d",pro);
-    printf("\nthe quotient of the two numbers is %d", quo);
+    printf("\nThe quotient of the two numbers is %d",quo);
+    printf("\nThe remainder of the two numbers is %d",rem);
     return 0;
 }
+
